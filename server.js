4df40b9036c6b2fb928738a7d1d3503932d2d1de
1egoman <rsg1egoman@gmail.com>
2016-05-05 20:58:00 -0400

    working on client code

--- server.js
+++ server.js
@@ -3,52 +3,6 @@ const phantom = require('phantom'),
       cheerio = require('cheerio'),
       Promise = require('bluebird');
 
-class PageLabels {
-  constructor(dom) {
-    this.dom = dom;
-    this.dollar = cheerio.load(this.dom);
-    this.labels = [];
-  }
-
-  isLabel(n) {
-    return n.indexOf("aria") === 0;
-  }
-
-  getLabelContents(name, node) {
-    switch (name) {
-      case "aria-label": return node.attribs["aria-label"];
-      case "aria-labelledby":
-        // let val = this.dollar(`#${node.attribs["aria-labelledby"]}`);
-        console.log(node)
-    }
-  }
-
-  // extract labels from nodes
-  getLabelFromNode(node, opts) {
-    let label = Object.keys(node.attribs).find(this.isLabel);
-    if (label) {
-      this.labels.push({
-        label,
-        value: this.getLabelContents(label, node),
-        opts,
-      });
-      this.getLabelFromNode(node.parent, {child: node});
-    } else {
-      return null;
-    }
-  }
-
-  parse() {
-    let $ = this.dollar;
-    return new Promise((resolve, reject) => {
-      $("[aria-label], [aria-labelledby]").toArray()
-      .forEach((i, ct) => {
-        this.getLabelFromNode(i);
-      });
-    });
-  }
-}
-
 // do a search with the page in mind
 let sitepage = null, phInstance = null;
 phantom.create().then(instance => {
@@ -58,11 +12,27 @@ phantom.create().then(instance => {
   sitepage = page;
   return page.open('https://twitter.com');
 }).then((data) => {
-  return sitepage.property('content');
+  return sitepage.evaluate(function() {
+    nodes = document.querySelectorAll("[aria-labelledby]");
+    total = Array.prototype.slice.call(nodes).map(function(i) {
+      label = document.getElementById(i.attributes["aria-labelledby"].nodeValue);
+      return {
+        node: i,
+        nodeText: i.innerText,
+        nodeAttr: i.attributes,
+        label: label,
+        labelText: label.innerText,
+      };
+    });
+
+    return total;
+  });
+  // return sitepage.property('content');
 }).then(content => {
-  let label = new PageLabels(content);
-  label.parse();
-  console.log(label.labels);
+  console.log("OUT", content);
+  // let label = new PageLabels(content);
+  // label.parse();
+  // console.log(label.labels);
 
   sitepage.close();
   phInstance.exit();
