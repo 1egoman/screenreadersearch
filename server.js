0df62773d413c20fa01e62e4c5553dff374d6849
1egoman <rsg1egoman@gmail.com>
2016-05-06 21:38:48 -0400

    tests and formatting code into a function

--- server.js
+++ server.js
@@ -17,75 +17,8 @@ let layout = `
 </div>
 `;
 
-let $ = require("cheerio").load(layout);
-let labels = [];
-
-// recursively crawl the dom tree to find the textual content of a node
-function getNodeContents(node) {
-  if (node.children) {
-    return node.children.reduce((total, child) => {
-      // recurse through all subnodes and put them into the tree
-      let data = getNodeContents(child);
-      if (Array.isArray(data)) {
-        return [...total, ...data];
-      } else {
-        return [...total, data];
-      }
-    }, []);
-  } else {
-    return node.data;
-  }
-}
-
-// return the label for a node
-function getNodeDescriptor(node) {
-  let nodeContent = getNodeContents(node).join(" ").trim();
-
-  // a normal label
-  if (node.attribs["aria-label"]) {
-    return {
-      label: node.attribs["aria-label"], // the label itself
-      nodeContent,
-    };
-
-  // a label that references another element
-  } else if (node.attribs["aria-labelled"]) {
-    let labelId = node.attribs["aria-labelledby"];
-    let labelledNode = $(`#${labelId}`).toArray();
-    if (labelledNode.length > 0) {
-      return {
-        type: "aria-labelledby",
-        nodeContent,
-        label: getNodeContents(labelledNode[0])
-               .join(" ")
-               .trim(),
-        labelRef: node.attribs["aria-labelledby"], // the id of the element that contains
-                                                // this element's label
-      };
-    } else {
-      return {
-        error: "NO_SUCH_LABEL_ID",
-        id: labelId,
-        nodeContent,
-      };
-    }
-  } else {
-    return {nodeContent, label: null};
-  }
-}
-
-// return the active state of a node. ie, is a checkbox checked?
-function getNodeState(node) {
-  let attr = node.attribs;
-  return attr.checked ||
-         attr.value ||
-         attr["aria-pressed"] ||
-         attr["aria-checked"] ||
-         attr["aria-selected"];
-}
-
-
-let selectors = {
+// the selectors to try when seaching for components
+const selectors = {
   button: [
     ".btn", ".button",
     "[role=button]",
@@ -100,36 +33,101 @@ let selectors = {
     "[role=textbox]",
     "input[type=text]",
   ],
-}, data = [];
+};
 
+module.exports = function editablePicker(layout) {
+  let $ = require("cheerio").load(layout);
+  let labels = [];
 
-// for each role, try and find matches
-for (let role in selectors) {
-  if (selectors.hasOwnProperty(role)) {
-    let selector = selectors[role].join(", ");
-    let output = $(selector).toArray().map(node => {
-      // get the text associated with a node
-      let content = getNodeDescriptor(node);
-
-      // return each node's data
-      return {
-        role, // the input type
-
-        // the text label for the element
-        label: content.label || content.nodeContent,
-
-        // the present condition of te element
-        state: getNodeState(node) || content.nodeContent, 
-      };
-    });
-
-    data = [...data, ...output];
+  // recursively crawl the dom tree to find the textual content of a node
+  function getNodeContents(node) {
+    if (node.children) {
+      return node.children.reduce((total, child) => {
+        // recurse through all subnodes and put them into the tree
+        let data = getNodeContents(child);
+        if (Array.isArray(data)) {
+          return [...total, ...data];
+        } else {
+          return [...total, data];
+        }
+      }, []);
+    } else {
+      return node.data;
+    }
   }
+
+  // return the label for a node
+  function getNodeDescriptor(node) {
+    let nodeContent = getNodeContents(node).join(" ").trim();
+
+    // a normal label
+    if (node.attribs["aria-label"]) {
+      return {
+        label: node.attribs["aria-label"], // the label itself
+        nodeContent,
+      };
+
+    // a label that references another element
+    } else if (node.attribs["aria-labelledby"]) {
+      let labelId = node.attribs["aria-labelledby"];
+      let labelledNode = $(`#${labelId}`).toArray();
+      if (labelledNode.length > 0) {
+        return {
+          type: "aria-labelledby",
+          nodeContent,
+          label: getNodeContents(labelledNode[0])
+                 .join(" ")
+                 .trim(),
+          labelRef: node.attribs["aria-labelledby"], // the id of the element that contains
+                                                     // this element's label
+        };
+      } else {
+        return {
+          error: "NO_SUCH_LABEL_ID",
+          id: labelId,
+          nodeContent,
+        };
+      }
+    } else {
+      return {nodeContent, label: null};
+    }
+  }
+
+  // return the active state of a node. ie, is a checkbox checked?
+  function getNodeState(node) {
+    let attr = node.attribs;
+    return attr.checked ||
+           attr.value ||
+           attr["aria-pressed"] ||
+           attr["aria-checked"] ||
+           attr["aria-selected"];
+  }
+
+
+  // for each role, try and find matches
+  let data = [];
+  for (let role in selectors) {
+    if (selectors.hasOwnProperty(role)) {
+      let selector = selectors[role].join(", ");
+      let output = $(selector).toArray().map(node => {
+        // get the text associated with a node
+        let content = getNodeDescriptor(node);
+
+        // return each node's data
+        return {
+          role, // the input type
+          content: content.nodeContent,
+
+          // the text label for the element
+          label: content.label || content.nodeContent,
+
+          // the present condition of te element
+          state: getNodeState(node),
+        };
+      });
+
+      data = [...data, ...output];
+    }
+  }
+  return data;
 }
-
-console.log(data)
-
-/*
- * Buttons: Click them to see what happens
- *
- */
---
+++ test/editablepicker.spec.js
@@ -0,0 +1,64 @@
+"use strict";
+let ep = require("../server"),
+    assert = require("assert");
+
+describe("editablepicker", function() {
+  this.slow(250);
+
+  describe("labels", function() {
+    it("should pull from aria-label", function() {
+      assert.deepEqual(
+        ep(`<div role="button" aria-label="my button label"></div>`),
+        [{role: "button", label: "my button label", content: "", state: undefined}]
+      );
+    });
+    it("should pull from aria-labelledby", function() {
+      assert.deepEqual(
+        ep(`<div id="my-element">my button label</div>
+            <div role="button" aria-labelledby="my-element"></div>`),
+        [{role: "button", label: "my button label", content: "", state: undefined}]
+      );
+    });
+    it("should use the first element with aria-labelledby when there's collisions", function() {
+      assert.deepEqual(
+        ep(`<div id="my-element">my button label</div>
+            <div id="my-element">another label</div>
+            <div role="button" aria-labelledby="my-element"></div>`),
+        [{role: "button", label: "my button label", content: "", state: undefined}]
+      );
+    });
+    it("should fail gracefully when a label doesn't exist", function() {
+      assert.deepEqual(
+        ep(`<div role="button" aria-labelledby="no-exist"></div>`),
+        [{role: "button", label: "", content: "", state: undefined}]
+      );
+    });
+    it("should fail gracefully without any label", function() {
+      assert.deepEqual(
+        ep(`<div role="button"></div>`),
+        [{role: "button", label: "", content: "", state: undefined}]
+      );
+    });
+  });
+
+  describe("buttons", function() {
+    it("should detect role=button", function() {
+      assert.deepEqual(
+        ep(`<div role="button" aria-label="my button label">my content</div>`),
+        [{role: "button", label: "my button label", content: "my content", state: undefined}]
+      );
+    });
+    it("should detect button element", function() {
+      assert.deepEqual(
+        ep(`<button aria-label="my button label">my content</button>`),
+        [{role: "button", label: "my button label", content: "my content", state: undefined}]
+      );
+    });
+    it("should detect input[type=submit]", function() {
+      assert.deepEqual(
+        ep(`<input type="submit" aria-label="my button label" />`),
+        [{role: "button", label: "my button label", content: "", state: undefined}]
+      );
+    });
+  });
+});
